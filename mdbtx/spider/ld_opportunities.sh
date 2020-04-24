0999671f9f8ee1b54f2656c22ab0c112ed626e32
Alex Infanzon <alexinfanzon@yahoo.com>
2020-04-24 11:59:11 -0700

    Delete ld_opportunities.sh

--- mdbtx/spider/ld_opportunities.sh
+++
@@ -1,8 +0,0 @@
-mysqlimport --user='root' \
-            --password='' \
-            --fields-terminated-by=, \
-            --lines-terminated-by='0x0d0a' \
-            --ignore-lines=1 \
-            --verbose \
-            --columns='id,accountName,name,owner,amount,closeDate,stageName' \
-test '/mdb/mdbtx/spider/opportunities.csv'
